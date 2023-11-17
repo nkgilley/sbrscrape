26353b8012fdd6939173e9ce8d837f0dfc1a6b91
Nolan Gilley <nkgilley@gmail.com>
2023-11-17 11:34:29 -0500

    fix scrambled data

--- src/sbrscrape/__init__.py
+++ src/sbrscrape/__init__.py
@@ -22,7 +22,7 @@ class Scoreboard:
             date = datetime.today().strftime("%Y-%m-%d")
         _line = 'currentLine' if current_line else 'openingLine'
 
-        spreads = moneylines = totals = []
+        spreads = moneylines = totals = {}
 
         spread_url = f"https://www.sportsbookreview.com/betting-odds/{sport_dict[sport]}/?date={date}"
         r = requests.get(spread_url)
@@ -31,39 +31,39 @@ class Scoreboard:
             build_id = json.loads(j[0])['buildId']
             spreads_url = f"https://www.sportsbookreview.com/_next/data/{build_id}/betting-odds/{sport_dict[sport]}.json?league={sport_dict[sport]}&date={date}"
             spreads_json = requests.get(spreads_url).json()
-            spreads = spreads_json['pageProps']['oddsTables'][0]['oddsTableModel']['gameRows']
+            spreads_list = spreads_json['pageProps']['oddsTables'][0]['oddsTableModel']['gameRows']
+            spreads = {g['gameView']['gameId']: g for g in spreads_list}
         except IndexError:
             return []
 
         moneyline_url = f"https://www.sportsbookreview.com/_next/data/{build_id}/betting-odds/{sport_dict[sport]}/money-line/full-game.json?league={sport_dict[sport]}&oddsType=money-line&oddsScope=full-game&date={date}"
         moneyline_json = requests.get(moneyline_url).json()
-        moneylines = moneyline_json['pageProps']['oddsTables'][0]['oddsTableModel']['gameRows']
-
+        moneylines_list = moneyline_json['pageProps']['oddsTables'][0]['oddsTableModel']['gameRows']
+        moneylines = {g['gameView']['gameId']: g for g in moneylines_list}
         totals_url = f"https://www.sportsbookreview.com/_next/data/{build_id}/betting-odds/{sport_dict[sport]}/totals/full-game.json?league={sport_dict[sport]}&oddsType=totals&oddsScope=full-game&date={date}"
         totals_json = requests.get(totals_url).json()
-        totals = totals_json['pageProps']['oddsTables'][0]['oddsTableModel']['gameRows']
+        totals_list = totals_json['pageProps']['oddsTables'][0]['oddsTableModel']['gameRows']
+        totals = {g['gameView']['gameId']: g for g in totals_list}
 
-        all_stats = []
-        for idx, game in enumerate(spreads):
-            all_stats.append(game)
-            all_stats[idx]['moneylines'] = moneylines[idx]
-            all_stats[idx]['totals'] = totals[idx]
+        all_stats = {
+            game_id: {'spreads': spreads[game_id], 'moneylines': moneylines[game_id], 'totals': totals[game_id], } for game_id in spreads.keys()
+        }
 
         games = []
-        for event in all_stats:
+        for event in all_stats.values():
             game = {}
-            game['date'] = event['gameView']['startDate']
-            game['status'] = event['gameView']['gameStatusText']
-            game['home_team'] = event['gameView']['homeTeam']['fullName']
-            game['home_team_loc'] = event['gameView']['homeTeam']['displayName']
-            game['home_team_abbr'] = event['gameView']['homeTeam']['shortName']
-            game['home_team_rank'] = event['gameView']['homeTeam']['rank']
-            game['away_team'] = event['gameView']['awayTeam']['fullName']
-            game['away_team_loc'] = event['gameView']['awayTeam']['displayName']
-            game['away_team_abbr'] = event['gameView']['awayTeam']['shortName']
-            game['away_team_rank'] = event['gameView']['awayTeam']['rank']
-            game['home_score'] = event['gameView']['homeTeamScore']
-            game['away_score'] = event['gameView']['awayTeamScore']
+            game['date'] = event['spreads']['gameView']['startDate']
+            game['status'] = event['spreads']['gameView']['gameStatusText']
+            game['home_team'] = event['spreads']['gameView']['homeTeam']['fullName']
+            game['home_team_loc'] = event['spreads']['gameView']['homeTeam']['displayName']
+            game['home_team_abbr'] = event['spreads']['gameView']['homeTeam']['shortName']
+            game['home_team_rank'] = event['spreads']['gameView']['homeTeam']['rank']
+            game['away_team'] = event['spreads']['gameView']['awayTeam']['fullName']
+            game['away_team_loc'] = event['spreads']['gameView']['awayTeam']['displayName']
+            game['away_team_abbr'] = event['spreads']['gameView']['awayTeam']['shortName']
+            game['away_team_rank'] = event['spreads']['gameView']['awayTeam']['rank']
+            game['home_score'] = event['spreads']['gameView']['homeTeamScore']
+            game['away_score'] = event['spreads']['gameView']['awayTeamScore']
             game['home_spread'] = {}
             game['home_spread_odds'] = {}
             game['away_spread'] = {}
@@ -73,8 +73,8 @@ class Scoreboard:
             game['total'] = {}
             game['home_ml'] = {}
             game['away_ml'] = {}
-            if 'oddsViews' in event:
-                for line in event['oddsViews']:
+            if 'spreads' in event:
+                for line in event['spreads']['oddsViews']:
                     if not line:
                         continue
                     game['home_spread'][line['sportsbook']] = line[_line]['homeSpread']
